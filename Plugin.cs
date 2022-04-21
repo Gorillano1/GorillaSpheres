afa350eeb07ca84cf0ae28f5fd51184340371441
Gorillano1 <104138117+Gorillano1@users.noreply.github.com>
2022-04-21 20:22:15 +1000

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,86 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+using System.IO;
+using System.Reflection;
+
+namespace gorillaspheres
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+
+        void OnEnable()
+        {
+            /* Set up your mod here */
+            /* Code here runs at the start and whenever your mod is enabled*/
+
+            HarmonyPatches.ApplyHarmonyPatches();
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnDisable()
+        {
+            /* Undo mod setup here */
+            /* This provides support for toggling mods with ComputerInterface, please implement it :) */
+            /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+            HarmonyPatches.RemoveHarmonyPatches();
+            Utilla.Events.GameInitialized -= OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+        }
+
+        void Update()
+        {
+            /* Code here runs every frame when the mod is enabled */
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is joined */
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            /* Activate your mod here */
+            /* This code will run regardless of if the mod is enabled*/
+
+            inRoom = true;
+            int ghand = LayerMask.NameToLayer("Gorilla Hand");
+            GameObject lefthand = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            GameObject hand = GameObject.Find("OfflineVRRig/Actual Gorilla/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L/palm.01.L");
+            lefthand.transform.SetParent(hand.transform, false);
+            lefthand.transform.localScale = new Vector3(0.09999999f, 0.09999999f, 0.09999999f);
+            lefthand.layer = ghand;
+            GameObject righthand = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            GameObject rhand = GameObject.Find("OfflineVRRig/Actual Gorilla/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R");
+            righthand.transform.SetParent(rhand.transform, false);
+            righthand.transform.localScale = new Vector3(0.09999999f, 0.09999999f, 0.09999999f);
+            righthand.layer = ghand;
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is left */
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            /* Deactivate your mod here */
+            /* This code will run regardless of if the mod is enabled*/
+
+            inRoom = false;
+            GameObject SphereR = GameObject.Find("OfflineVRRig/Actual Gorilla/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R/Sphere");
+            Destroy(SphereR);
+            GameObject SphereL = GameObject.Find("OfflineVRRig/Actual Gorilla/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L/palm.01.L/Sphere");
+            Destroy(SphereL);
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace gorillaspheres
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.xemply.gorillatag.gorillaspheres";
+        public const string Name = "gorillaspheres";
+        public const string Version = "1.0.0";
+    }
+}
